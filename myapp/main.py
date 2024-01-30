b160d5c09f3198cc2b2a3dfd1e2d20fae651ed64
Flamur64 <136818887+Flamur64@users.noreply.github.com>
2024-01-30 13:41:05 +0100

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -1,11 +1,8 @@
-import requests
-import numpy as np
+import sys
 
-def calculate_mean(data):
-    return np.mean(data)
+def main():
+    args = sys.argv[1:]
+    print("Received arguments:", args)
 
 if __name__ == "__main__":
-    response = requests.get('https://api.exchangeratesapi.io/latest')
-    rates = response.json()['rates']
-    mean_rate = calculate_mean(list(rates.values()))
-    print(f"Der durchschnittliche Wechselkurs ist: {mean_rate}")
+    main()
